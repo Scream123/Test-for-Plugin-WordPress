ab145ac9cfac697b8d7e99fac498bdefafe9e267
Vladimir <fly_life@mail.ru>
2018-08-03 18:26:17 +0300

    Plugin Custom Post Type and Taxonomy for WP

---
+++ include/page-vm_cars.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Template part for displaying posts
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.2
+ */
+
+?>
+
+<!--<article id="post---><?php //the_ID(); ?><!--" --><?php //post_class(); ?><!-->-->
+<!--    <h1>--><?php //the_title();?><!--</h1>-->
+<!--    <div>--><?php //the_content(); ?><!--</div>-->
+<!--</article><!-- #post-## -->-->
